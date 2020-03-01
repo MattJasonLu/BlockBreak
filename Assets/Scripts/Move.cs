861873d7647217228f69514725498af77f489434
MurderousCrotch <1132464757@qq.com>
2020-03-01 10:53:37 +0900

    完成

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// 移动速度
+	public float speed = 5;
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		// 移动位置
+		// Time.deltaTime ->  1秒50帧 1/50  -> 1M/s
+		transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	// 设置子弹的初始速度
+	public float speed = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject bulletGo = Instantiate(bullet, transform.position, Quaternion.identity);
+			// 获取刚体组件
+			Rigidbody rgd = bulletGo.GetComponent<Rigidbody>();
+			rgd.velocity = transform.forward * speed;
+        }
+    }
+}
